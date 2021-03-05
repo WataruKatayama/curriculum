03bb8fa7a67a31556b20dfac47589a7c9ed5ef84
WataruKatayama <72663185+WataruKatayama@users.noreply.github.com>
2021-03-05 23:05:31 +0900

    課題提出テスト
    1-3
    index.html
    main.js　の提出

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,22 @@
+//問１
+let a =! 10;
+
+let b;
+10<=b<20;
+
+
+//問２
+let x = 10;
+if (10<=x && x<=20) {
+    console.log('成功です');
+} else {
+    console.log('失敗です');
+}
+
+//問３
+let y = 0;
+if (y%2==0) {
+    console.log('偶数です');
+} else {
+    console.log('奇数です');
+}
